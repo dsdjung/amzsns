dd5cfebe893e749bbf24217e36559291bc17b2dd
dsdjung <dsdjung@users.noreply.github.com>
2014-08-07 10:07:50 -0700

    Create amzsns.go

---
+++ amzsns.go
@@ -0,0 +1,112 @@
+// Copyright 2011-2013 Claypost Inc
+// Use of this source code is governed by an MIT-style license
+// that can be found in the LICENSE file.
+// The code is largely based on amzses from Numrotron Inc written by Patrick Crosby
+
+package amzsns
+
+import (
+    "crypto/hmac"
+    "crypto/sha256"
+    "encoding/base64"
+    "errors"
+    "fmt"
+    "github.com/stathat/jconfig"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "net/url"
+    "strings"
+    "time"
+)
+
+const (
+    endpoint = "http://sns.us-west-2.amazonaws.com"
+)
+
+var accessKey, secretKey string
+
+func init() {
+    config := jconfig.LoadConfig("/etc/aws.conf")
+    accessKey = config.GetString("aws_access_key")
+    secretKey = config.GetString("aws_secret_key")
+}
+
+func CreateEndPoint(platformApplicationARN, customerUserData, token string) (string, error) {
+    now := time.Now().UTC()
+    // date format: "Tue, 25 May 2010 21:20:27 +0000"
+    date := now.Format("Mon, 02 Jan 2006 15:04:05 -0700")
+    
+    h := hmac.New(sha256.New, []uint8(secretKey))
+    h.Write([]uint8(date))
+    signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
+    //auth := fmt.Sprintf("AWS3-HTTPS AWSAccessKeyId=%s, Algorithm=HmacSHA256, Signature=%s", accessKey, signature)
+    
+    data := make(url.Values)
+    data.Add("Action", "CreatePlatformEndpoint")
+    data.Add("SignatureMethod", "HmacSHA256")
+    data.Add("PlatformApplicationArn", platformApplicationARN)
+    data.Add("CustomUserData", customerUserData)
+    data.Add("Token", token)
+    data.Add("AWSAccessKeyId", accessKey)
+    data.Add("SignatureVersion", "2")
+    data.Add("Signature", signature)
+    data.Add("Version", "2010-03-31")
+    data.Add("Timestamp", date)
+
+    return snsPost(data)
+}
+
+
+func PublishMobile(targetARN, message) (string, error) {
+    now := time.Now().UTC()
+    // date format: "Tue, 25 May 2010 21:20:27 +0000"
+    date := now.Format("Mon, 02 Jan 2006 15:04:05 -0700")
+    
+    h := hmac.New(sha256.New, []uint8(secretKey))
+    h.Write([]uint8(date))
+    signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
+    //auth := fmt.Sprintf("AWS3-HTTPS AWSAccessKeyId=%s, Algorithm=HmacSHA256, Signature=%s", accessKey, signature)
+    
+    data := make(url.Values)
+    data.Add("Action", "Publish")
+    data.Add("TargetArn", targetARN)
+    data.Add("Message", message)
+    data.Add("Token", token)
+    data.Add("MessageStructure", "json")
+    data.Add("SignatureMethod", "HmacSHA256")
+    data.Add("AWSAccessKeyId", accessKey)
+    data.Add("SignatureVersion", "2")
+    data.Add("Signature", signature)
+    data.Add("Version", "2010-03-31")
+    data.Add("Timestamp", date)
+
+    return snsPost(data)
+}
+
+func snsPost(data url.Values) (string, error) {
+    body := strings.NewReader(data.Encode())
+    req, err := http.NewRequest("POST", endpoint, body)
+    if err != nil {
+        return "", err
+    }
+    req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+    r, err := http.DefaultClient.Do(req)
+    if err != nil {
+        log.Printf("http error: %s", err)
+        return "", err
+    }
+
+    resultbody, _ := ioutil.ReadAll(r.Body)
+    r.Body.Close()
+
+    if r.StatusCode != 200 {
+        log.Printf("error, status = %d", r.StatusCode)
+
+        log.Printf("error response: %s", resultbody)
+        return "", errors.New(fmt.Sprintf("error code %d. response: %s", r.StatusCode, resultbody))
+    }
+
+    return string(resultbody), nil
+}
